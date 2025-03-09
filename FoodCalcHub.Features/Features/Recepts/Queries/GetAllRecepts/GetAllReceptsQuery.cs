1135c2e89c05ab1f25727b35b2e4cc306398ca0b
Jeroen <jeroen_vloon@msn.com>
2025-03-09 13:14:31 +0100

    deletes

--- FoodCalcHub.Features/Features/Recepts/Queries/GetAllRecepts/GetAllReceptsQuery.cs
+++
@@ -1,6 +0,0 @@
-using ErrorOr;
-using FoodHub.Persistence.Entities;
-using MediatR;
-
-namespace FoodCalc.Features.Recepts.Queries.GetAllRecepts;
-public record GetAllReceptsQuery() : IRequest<ErrorOr<List<Recept>>>;
