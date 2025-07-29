f5b6da32837f3cdcc2032aba837658e1879b974c
Jeroen <jeroen_vloon@msn.com>
2025-07-29 20:38:36 +0200

    fixed bug that ingredient was null

--- FoodHub.DTOs/IngredientDto.cs
+++ FoodHub.DTOs/IngredientDto.cs
@@ -5,6 +5,4 @@ public class IngredientDto
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public bool ShouldBeAddedToShoppingCart { get; set; } = true;
-    public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set; }
 }
--- FoodHub.DTOs/RecipeIngredientDto.cs
+++ FoodHub.DTOs/RecipeIngredientDto.cs
@@ -2,12 +2,10 @@ namespace FoodHub.DTOs;
 
 public class RecipeIngredientDto
 {
-    public Guid Id { get; set; }
+	public Guid Id { get; set; } = new Guid();
     public Guid RecipeId { get; set; }
     public Guid IngredientId { get; set; }
-    public IngredientDto Ingredient { get; set; } = new();
+    public IngredientDto? Ingredient { get; set; }
     public decimal Amount { get; set; }
     public IngredientAmountTypeDto IngredientAmount { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set; }
 }
