9553b794792145bf6cd360928fe9eaa1c17a574c
Jeroen <jeroen_vloon@msn.com>
2025-07-29 21:30:15 +0200

    decimal extention for formatting of decimals removed remove button from printing page

--- FoodHub.DTOs/RecipeIngredientDto.cs
+++ FoodHub.DTOs/RecipeIngredientDto.cs
@@ -1,11 +1,13 @@
+using System.Xml.Linq;
+
 namespace FoodHub.DTOs;
 
 public class RecipeIngredientDto
 {
 	public Guid Id { get; set; } = new Guid();
-    public Guid RecipeId { get; set; }
-    public Guid IngredientId { get; set; }
-    public IngredientDto? Ingredient { get; set; }
-    public decimal Amount { get; set; }
-    public IngredientAmountTypeDto IngredientAmount { get; set; }
+	public Guid RecipeId { get; set; }
+	public Guid IngredientId { get; set; }
+	public IngredientDto? Ingredient { get; set; }
+	public decimal Amount { get; set; }
+	public IngredientAmountTypeDto IngredientAmount { get; set; }
 }
